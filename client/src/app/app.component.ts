1b27778138924a928786a8d2bcc5ad6fdfbd8033
jdbbaugh <jbaugh78@msn.com>
2019-05-20 09:33:05 -0500

    didn't get too far

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Muserk\'s Interview Challenge!';
+  selectedFile: File = null;
+  constructor(private http: HttpClient) {}
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+    console.log(event);
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('text/csv', this.selectedFile, this.selectedFile.name);
+    this.http.post('https://localhost/api/uploads', fd, {
+
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(event => {
+        if(event.type === HttpEventType.UploadProgress) {
+          console.log('Upload Progress ' + Math.round(event.loaded / event.total) * 100 + '%')
+        } else if(event.type === HttpEventType.Response){
+          console.log(event);
+        }
+      })
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFileUploaderModule } from "angular-file-uploader";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    AngularFileUploaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
